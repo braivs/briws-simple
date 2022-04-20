6582b345e6aa5d2b311125622cf2bdf15939c05f
Briws <brightwiths@gmail.com>
2022-04-20 14:12:20 +0300

    v0.1.15

--- index.tsx
+++
@@ -1,1 +0,0 @@
-export { Briws } from "./src"
--- src/components/Briws.tsx
+++ src/components/Briws.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Briws() {
     return (
         <div className="App">
-            Hi I am simple TS test from briws-simple!!!
+            Hi I am simple TS test from briws-simple package.
         </div>
     )
 }
